5d2c1fd3f182057f9837c4a1330e00418438acfd
root <you@example.com>
2023-01-24 16:21:02 +0000

    1-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,13 +3,9 @@
 """Define a class Square."""
 
 
-
-
-
 class Square:
 
     """Represent a square."""
-
 
 
     def __init__(self, size=0):
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,20 +3,14 @@
 """Define a class Square."""
 
 
-
-
-
 class Square:
 
     """Represent a square."""
 
 
-
     def __init__(self, size=0):
 
         """Initialize a new square.
-
-
 
         Args:
 
